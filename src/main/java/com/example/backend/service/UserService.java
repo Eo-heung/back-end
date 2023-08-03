cb515868fe369225b82853dd508fe3ec0a9c502c
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-08-03 16:13:51 +0900

    feat-9: 로그인 유효성 검증 및 로그인 기능 구현

---
+++ src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.backend.service;
+
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public User login(String userId, String userPw){
+        Optional<User> loginUser = userRepository.findByUserId(userId);
+
+        if(loginUser.isEmpty()) {
+            throw new RuntimeException("id not exist");
+        }
+
+        if(!passwordEncoder.matches(userPw, loginUser.get().getUserPw())) {
+            throw new RuntimeException("wrong pw");
+        }
+
+        return loginUser.get();
+    }
+
+
+    public User join(User user) {
+
+        return userRepository.save(user);
+    }
+
+}
