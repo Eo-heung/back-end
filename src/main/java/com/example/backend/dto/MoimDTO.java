13468865261c27b0d808beeb337f4b55c9c2b333
dxnx-hhh <dxnxhb@gmail.com>
2023-08-09 15:37:35 +0900

    feat-80: entity 세팅 및 모임 등록 기능 1차 구현

---
+++ src/main/java/com/example/backend/dto/MoimDTO.java
@@ -0,0 +1,56 @@
+package com.example.backend.dto;
+
+import com.example.backend.entity.Moim;
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MoimDTO {
+
+    private int moimId;
+    private String userId;
+    private LocalDateTime moimRegdate;
+    private String moimTitle;
+    private String moimContent;
+    private String onOff;
+    private String moimAddr1;
+    private String moimAddr2;
+    private String moimAddr3;
+    private String limitAge;
+    private String maxMoimUser;
+    private String cost;
+    private String isDelete;
+    private String isEnd;
+
+
+
+    public Moim DTOToEntity() {
+        return Moim.builder()
+                .moimId(this.moimId)
+                .userId(
+                        User.builder()
+                                .userId(this.userId)
+                                .build()
+                )
+                .moimRegdate(LocalDateTime.now())
+                .moimTitle(this.moimTitle)
+                .moimContent(this.moimContent)
+                .onOff(this.onOff)
+                .moimAddr1(this.moimAddr1)
+                .moimAddr2(this.moimAddr2)
+                .moimAddr3(this.moimAddr3)
+                .limitAge(this.limitAge)
+                .maxMoimUser(this.maxMoimUser)
+                .cost(this.cost)
+                .isDelete(this.isDelete)
+                .isEnd(this.isEnd)
+                .build();
+    }
+}
--- src/main/java/com/example/backend/entity/User.java
+++ src/main/java/com/example/backend/entity/User.java
@@ -109,5 +109,7 @@ public class User {
                 .updateDatetime(this.updateDatetime)
                 .build();
     }
+
+
 }
 
