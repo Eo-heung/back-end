d9894cf8a2954c520068d788c9ab3ac22d7d445e
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-08-16 10:22:15 +0900

    feat-32: 소개글 작성 기능 구현.

--- src/main/java/com/example/backend/controller/MyPageController.java
+++ src/main/java/com/example/backend/controller/MyPageController.java
@@ -59,6 +59,64 @@ public class MyPageController {
         }
     }
 
+    @PostMapping("/mymoim")
+    public ResponseEntity<?> getUserMoim(@RequestHeader("Authorization") String token) {
+        ResponseDTO<Moim> responseDTO = new ResponseDTO<>();
+
+        try {
+            String userId = jwtTokenProvider.validateAndGetUsername(token);
+            User user = userRepository.findByUserId(userId).get();
+
+            List<Moim> moimList = moimRepository.findByUserId(user);
+
+            responseDTO.setItems(moimList);
+            responseDTO.setStatusCode(HttpStatus.OK.value());
+            return ResponseEntity.ok().body(responseDTO);
+
+        } catch (Exception e) {
+            responseDTO.setErrorMessage(e.getMessage());
+            responseDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+
+    @PostMapping("/editinfo")
+    public ResponseEntity<?> editInfo(@RequestHeader("Authorization") String token, @RequestBody UserDTO userDTO) {
+        ResponseDTO<UserDTO> responseDTO = new ResponseDTO<>();
+
+        try {
+            String userId = jwtTokenProvider.validateAndGetUsername(token);
+            User user = userRepository.findByUserId(userId).get();
+
+            user.setUserName(userDTO.getUserName());
+            user.setUserAddr1(userDTO.getUserAddr1());
+            user.setUserAddr2(userDTO.getUserAddr2());
+            user.setUserAddr3(userDTO.getUserAddr3());
+            user.setUserHobby1(userDTO.getUserHobby1());
+            user.setUserHobby2(userDTO.getUserHobby2());
+            user.setUserHobby3(userDTO.getUserHobby3());
+            user.setUserRecommend(userDTO.getUserRecommend());
+            user.setUserStatusMessage(userDTO.getUserStatusMessage());
+            user.setUserUpdate(LocalDateTime.now());
+
+
+            //회원정보수정(화면에서 보내준 내용을 디비에 저장)
+            User editUser = myPageService.editInfo(user);
+            UserDTO editUserDTO = editUser.EntityToDTO();
+            editUserDTO.setUserPw("");
+
+            responseDTO.setItem(editUserDTO);
+            responseDTO.setStatusCode(HttpStatus.OK.value());
+
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            responseDTO.setErrorMessage(e.getMessage());
+            responseDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
     @PostMapping("/changephone")
     public ResponseEntity<?> changeNum(@RequestHeader("Authorization") String token, @RequestBody UserDTO userDTO) {
         ResponseDTO<UserDTO> responseDTO = new ResponseDTO<>();
