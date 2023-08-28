a31c07f117dae1227fb7fccbe76b653e9b085d9f
zener9233 <cite9233@naver.com>
2023-08-28 17:14:44 +0900

    feat-33 : totalGam의 개수를 front로 보내는 로직 추가

--- src/main/java/com/example/backend/controller/PaymentController.java
+++ src/main/java/com/example/backend/controller/PaymentController.java
@@ -87,13 +87,26 @@ public class PaymentController {
 
     @PostMapping("/paymentList")
     public ResponseEntity<?> PaymentList(@RequestHeader("Authorization") String token) {
-        System.out.println("#################");
         ResponseDTO<PaymentGam> responseDTO = new ResponseDTO<>();
         String userId = jwtTokenProvider.validateAndGetUsername(token);
 
         List<PaymentGam> payments = paymentRepository.findByUserIdOrderByPayDateDesc(userId);
 
         responseDTO.setItems(payments);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @PostMapping("/totalGotGam")
+    public ResponseEntity<?> totalGotGam(@RequestHeader("Authorization") String token) {
+        System.out.println("#################");
+        ResponseDTO<Long> responseDTO = new ResponseDTO<>();
+        String userId = jwtTokenProvider.validateAndGetUsername(token);
+
+        User user = userRepository.findByUserId(userId).get();
+        Long totalGam = user.getTotalGam();
+        System.out.println(totalGam);
+
+        responseDTO.setItem(totalGam);
         return ResponseEntity.ok(responseDTO);
     }
 
